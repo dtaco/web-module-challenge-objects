c11e0929ea480ade867bf0223e87acf5a71198e1
David Tacoronte <davidtacoronte@Davids-MBP.attlocal.net>
2022-08-08 11:31:43 -0400

    Completed task 6 and 7

--- index.js
+++ index.js
@@ -136,8 +136,8 @@ Use the getReviewByIndex function below to do the following:
 */
 
 
-function getReviewByIndex(/*Your code here*/) {
-  /*Your code here*/
+function getReviewByIndex(array, reviewIndex) {
+  return `${array[reviewIndex].name} gave the restaurant a ${array[reviewIndex].rating} star review, and their feedback was: ${array[reviewIndex].feedback}`
 }
 
   
@@ -155,8 +155,8 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
+function getLastReview(array) {
+  return `${array[array.length-1].name} gave the restaurant a ${array[array.length-1].rating} star review, and their feedback was: ${array[array.length-1].feedback}`
 } 
 
 
